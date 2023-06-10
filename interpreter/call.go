ce1b93d143687f12879214fc470821125a3da258
Ryan Andersen <redninja106@live.com>
2023-06-10 18:41:55 -0400

    indirect calls

--- interpreter/call.go
+++ interpreter/call.go
@@ -11,8 +11,16 @@ type Call struct {
 
 func init() {
 	RegisterInstruction("call", func(fields []string) Instruction {
+		var identifier string
+
+		if len(fields) > 1 {
+			identifier = fields[1]
+		} else {
+			identifier = ""
+		}
+
 		return Call{
-			Identifier: fields[1],
+			Identifier: identifier,
 		}
 	})
 	RegisterInstruction("", func(fields []string) Instruction {
@@ -26,10 +34,23 @@ func (c Call) execute(ctx *Context) {
 	ctx.LocalStack.Push(make(map[string]int64))
 	ctx.ReturnStack.Push(ctx.Pointer)
 
-	val, ok := ctx.FuncMap[c.Identifier]
-	if !ok {
-		panic(errors.New(fmt.Sprintf("unknown function %s", c.Identifier)))
-	}
+	if c.Identifier == "" {
+		addr := ctx.ExecutionStack.Pop()
 
-	ctx.Pointer = val
+		// make sure it's a func we're calling
+		_, ok := ctx.Instructions[addr].(Func)
+
+		if !ok {
+			panic("Indirect call tried to call a non-function address!")
+		}
+
+		ctx.Pointer = addr
+	} else {
+		val, ok := ctx.FuncMap[c.Identifier]
+		if !ok {
+			panic(errors.New(fmt.Sprintf("unknown function %s", c.Identifier)))
+		}
+
+		ctx.Pointer = val
+	}
 }
